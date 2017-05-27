65d3fc47d6dbc516c1c2eee10fe9a369275be559
Manikandan Sundararajan <mtiruman@buffalo.edu>
2017-05-27 02:43:41 -0400

    Can get song title and artist name from spotify

---
+++ content.js
@@ -0,0 +1,13 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      // var firstHref = $("a[href^='http']").eq(0).attr("href");
+      // var firstHref = $("div[class^='track-info__name']>div[class^=react-contextmenu-wrapper]+a[href^=/album]")
+      var songName = $("#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__name > div > a").text()
+      var songArtist = $("#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__artists > span > span > a").text()
+      alert(songName)
+      alert(songArtist)
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": songName});
+    }
+  }
+);
