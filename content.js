24d10459d961aa8ba83b74795e93810915501d68
Manikandan Sundararajan <mtiruman@buffalo.edu>
2017-09-08 21:19:12 -0400

    Back after a long time. Minor changes

--- content.js
+++ content.js
@@ -1,12 +1,17 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      // var firstHref = $("a[href^='http']").eq(0).attr("href");
-      // var firstHref = $("div[class^='track-info__name']>div[class^=react-contextmenu-wrapper]+a[href^=/album]")
-      var songName = $("#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__name > div > a").text()
-      var songArtist = $("#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__artists > span > span > a").text()
-      alert(songName)
-      alert(songArtist)
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello"){
+    	var sname = document.querySelector('#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__name.ellipsis-one-line > div > a');
+    	sname = sname.innerHTML;
+    	var aname = document.querySelectorAll('#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__artists.ellipsis-one-line a');
+    	// console.log(artistellipsis.innerHTML);
+        var anameList = [];
+        for (i = 0; i < aname.length; i++){
+            anameList.push(aname[i].innerHTML);
+        }
+        sendResponse({songName: sname, artistName: anameList});
     }
-  }
-);
+  });
--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(){
-  var songName = $("#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__name > div > a").text();
-  var songArtist = $("#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__artists > span > span > a").text();
-  var p1 = document.getElementById("p1").innerHTML;
-  var p2 = document.getElementById("p2").innerHTML;
-  p1.innerHTML = songName;
-  p2.innerHTML = songArtist;
-  alert(document.getElementById("p1").innerHTML);
-  // var spot = document.getElementById('checkSpot');
-});
+window.onload = function(){
+	document.getElementById('checkSpot').onclick = function fun() {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+			chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response){
+				// alert(response.songName);
+				var songfield = document.getElementById('songName');
+				var artistField = document.getElementById('artistName');
+				songfield.textContent = response.songName;
+				artistField.textContent = "".concat(...response.artistName);
+			});
+		});
+	}
+}
