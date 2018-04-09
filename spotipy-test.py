4e93d4a19d027bf6acb63192672ac345deb6761b
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-04-09 17:12:07 -0400

    Infinite loop
    Making some changes so that the program can run forever and put the token gathering code into a separate function

--- spotipy-test.py
+++ spotipy-test.py
@@ -4,8 +4,16 @@ import json
 import spotipy
 import spotipy.util as util
 
-params = json.load(open('client_secrets.json'))
-scope = 'user-read-currently-playing'
+def load_params_and_get_token():
+    params = json.load(open('client_secrets.json'))
+    scope = 'user-read-currently-playing'
+    auth_token = util.prompt_for_user_token(params['username'], scope, 
+                        client_id=params['client_id'],
+                        client_secret=params['client_secret'],
+                        redirect_uri=params['redirect_uri']
+                        )
+    return auth_token
+
 
 # if len(sys.argv) > 1:
 #     username = sys.argv[1]
@@ -13,22 +21,19 @@ scope = 'user-read-currently-playing'
 #     print("Usage: %s username" % (sys.argv[0],))
 #     sys.exit()
 
-token = util.prompt_for_user_token(params['username'], scope, 
-                        client_id=params['client_id'],
-                        client_secret=params['client_secret'],
-                        redirect_uri=params['redirect_uri']
-                        )
-
 # print(spotipy.VERSION)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_playing_track()
-    progress = results['progress_ms']
-    duration = results['item']['duration_ms']
-    print("Progress seconds - ", progress/1000)
-    print("Duration seconds - ", duration/1000)
-    pprint.pprint(results['item']['name'])
-    
-else:
-    print("Can't get token for", username)
+token = load_params_and_get_token()
+
+for:
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.current_user_playing_track()
+        progress = results['progress_ms']
+        duration = results['item']['duration_ms']
+        print("Progress seconds - ", progress/1000)
+        print("Duration seconds - ", duration/1000)
+        pprint.pprint(results['item']['name'])
+    else:
+        print("Can't get token")
+        sys.exit()
