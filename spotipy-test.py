a637cd6e676cb4d1a0374490c46a442ff7767e18
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-04-10 01:40:02 -0400

    Extract artist name and display it

--- spotipy-test.py
+++ spotipy-test.py
@@ -20,10 +20,14 @@ def main():
         sp = spotipy.Spotify(auth=token)
         try:
             results = sp.current_user_playing_track()
+
+            artist_name = results['item']['album']['artists'][0]['name']
+            song_name = results['item']['name']
+
             current_progress = results['progress_ms']
             total_duration = results['item']['duration_ms']
             progress = round((current_progress/total_duration) * 100, 2)
-            print("\r{} - {}%".ljust(50,' ').format(results['item']['name'], progress), end='')
+            print("\r{} by {} - {}%".ljust(50,' ').format(song_name, artist_name, progress), end='')
         except TypeError:
             print("\rNo Track is playing", end='')
     else:
