1f9a6fe29a46dfd67635756098de1cb5835c69e3
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-09-28 21:59:10 -0400

    Restructure main function to avoid repeated token initialization

--- spotify-lyrics.py
+++ spotify-lyrics.py
@@ -21,54 +21,53 @@ def load_params_and_get_token():
     return auth_token
 
 
-def main(curr_tr):
-    token = load_params_and_get_token()
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        try:
-            results = sp.currently_playing()
-            artist_name = results["item"]["album"]["artists"][0]["name"]
-            song_name = results["item"]["name"]
-            current_progress = results["progress_ms"]
-            total_duration = results["item"]["duration_ms"]
-            progress = round((current_progress / total_duration) * 100, 2)
-            if song_name != curr_tr:
-                lyrics = az.extract_lyrics(artist_name, song_name)
-                # full_lyrics = '\n'.join(lyrics)
-                print(
-                    Fore.RED
-                    + "\r{} by {}\n\n".ljust(10, " ").format(song_name, artist_name),
-                    end="",
-                )
-                # print(full_lyrics + '\n')
-                az.pretty_print_lyrics(lyrics)
-                print(
-                    Fore.GREEN + "\rProgress - {}%".ljust(20, " ").format(progress),
-                    end="",
-                )
-                return song_name
-            else:
-                print(
-                    Fore.GREEN + "\rProgress - {}%".ljust(20, " ").format(progress),
-                    end="",
-                )
-                return song_name
-        except TypeError:
-            print(Fore.RED + "\rNo Track is playing", end="")
-    else:
-        print("Can't get token")
+def main(curr_tr, sp):
+    try:
+        results = sp.currently_playing()
+        artist_name = results["item"]["album"]["artists"][0]["name"]
+        song_name = results["item"]["name"]
+        current_progress = results["progress_ms"]
+        total_duration = results["item"]["duration_ms"]
+        progress = round((current_progress / total_duration) * 100, 2)
+        if song_name != curr_tr:
+            lyrics = az.extract_lyrics(artist_name, song_name)
+            # full_lyrics = '\n'.join(lyrics)
+            print(
+                Fore.RED
+                + "\r{} by {}\n\n".ljust(10, " ").format(song_name, artist_name),
+                end="",
+            )
+            # print(full_lyrics + '\n')
+            az.pretty_print_lyrics(lyrics)
+            print(
+                Fore.GREEN + "\rProgress - {}%".ljust(20, " ").format(progress), end=""
+            )
+            return song_name
+        else:
+            print(
+                Fore.GREEN + "\rProgress - {}%".ljust(20, " ").format(progress), end=""
+            )
+            return song_name
+    except TypeError:
+        print(Fore.RED + "\rNo Track is playing", end="")
 
 
 if __name__ == "__main__":
     init(autoreset=True)
     curr_track = ""
-    while True:
-        try:
-            curr_track = main(curr_track)
-            # time.sleep(10)
-        except KeyboardInterrupt:
-            print("\b\b  \n\rShutting Down")
+
+    token = load_params_and_get_token()
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        while True:
             try:
-                sys.exit(0)
-            except SystemExit:
-                os._exit(0)
+                curr_track = main(curr_track, sp)
+                time.sleep(10)
+            except KeyboardInterrupt:
+                print("\b\b  \n\rShutting Down")
+                try:
+                    sys.exit(0)
+                except SystemExit:
+                    os._exit(0)
+    else:
+        print("Can't get token")
